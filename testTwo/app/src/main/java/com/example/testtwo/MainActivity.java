61a65db93596dab12fb2a8b12097b7b964b8775f
kushalb-dev <kushalb.dev@gmail.com>
2026-03-11 18:00:27 +0545

    commiting all files

---
+++ testTwo/app/src/main/java/com/example/testtwo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testtwo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText("Hello, Android Developers!");
+    }
+}
