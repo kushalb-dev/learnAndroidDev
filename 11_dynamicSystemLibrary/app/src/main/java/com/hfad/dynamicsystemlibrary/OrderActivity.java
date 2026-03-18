87b415e0e49c13d21d20aaeba8fd812a02f54f24
kushalb-dev <kushalb.dev@gmail.com>
2026-03-18 17:18:25 +0545

    added snackbar and fab with undo functionality

--- 11_dynamicSystemLibrary/app/src/main/java/com/hfad/dynamicsystemlibrary/OrderActivity.java
+++ 11_dynamicSystemLibrary/app/src/main/java/com/hfad/dynamicsystemlibrary/OrderActivity.java
@@ -1,6 +1,9 @@
 package com.hfad.dynamicsystemlibrary;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class OrderActivity extends AppCompatActivity {
+
+
+    private String oldOrderName = "";
+    private String oldOrderDetails = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +38,24 @@ public class OrderActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle(R.string.create_order);
         }
+        FloatingActionButton floatingActionButton = findViewById(R.id.floating_button_update_edit);
+        EditText editOrderName = findViewById(R.id.edit_order_name);
+        EditText editOrderDetails = findViewById(R.id.edit_order_details);
+        floatingActionButton.setOnClickListener(v -> {
+            Snackbar snackbar = Snackbar.make(v, R.string.snackbar_order_updated, Snackbar.LENGTH_LONG);
+            final String nameSnapshot = oldOrderName;
+            final String detailsSnapshot = oldOrderDetails;
+            snackbar.show();
+            oldOrderName = editOrderName.getText().toString();
+            oldOrderDetails = editOrderDetails.getText().toString();
+            snackbar.setAction("Undo", v1 -> {
+                editOrderDetails.setText(nameSnapshot);
+                editOrderName.setText(detailsSnapshot);
+                oldOrderName = nameSnapshot;
+                oldOrderDetails = nameSnapshot;
+                Toast toast = Toast.makeText(OrderActivity.this, R.string.toast_undone, Toast.LENGTH_SHORT);
+                toast.show();
+            });
+        });
     }
 }
