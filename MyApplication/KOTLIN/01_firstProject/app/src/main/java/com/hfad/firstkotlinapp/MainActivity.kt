147a4a2b61822858b0a47f62b7924cb5a7650342
kushalb-dev <kushalb.dev@gmail.com>
2026-03-19 17:01:34 +0545

    removed some unnecessary imports

--- MyApplication/KOTLIN/01_firstProject/app/src/main/java/com/hfad/firstkotlinapp/MainActivity.kt
+++ MyApplication/KOTLIN/01_firstProject/app/src/main/java/com/hfad/firstkotlinapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.hfad.firstkotlinapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
