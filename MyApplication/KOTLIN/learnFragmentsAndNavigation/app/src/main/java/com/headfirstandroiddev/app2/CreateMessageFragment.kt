e3f29278b59169ffaee7b4cb0c19bd266cef50e5
kushalb-dev <kushalb.dev@gmail.com>
2026-03-22 12:11:31 +0545

    learned how to navigate between fragments using nav graph

---
+++ MyApplication/KOTLIN/learnFragmentsAndNavigation/app/src/main/java/com/headfirstandroiddev/app2/CreateMessageFragment.kt
@@ -0,0 +1,17 @@
+package com.headfirstandroiddev.app2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class CreateMessageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_message, container, false)
+    }
+}
